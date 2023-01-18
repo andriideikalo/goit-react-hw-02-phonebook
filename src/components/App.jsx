fb3ca9dbaecaefe04cf7b801d52866e418655ade
andriideikalo <wlada801@gmail.com>
2023-01-18 23:28:53 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,47 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
 import ContactForm from './ContactForms/ContactForm';
+import { nanoid } from 'nanoid';
+// import { render } from '@testing-library/react';
 
-export const App = () => {
-  // state = {
-  //   contacts: [],
-  //   name: '',
-  // };
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
 
-  return (
-    <ContactForm />
+  addContact = contact => {
+    const id = nanoid();
+    const { name } = contact;
 
-    // <div
-    //   style={{
-    //     height: '100vh',
-    //     display: 'flex',
-    //     justifyContent: 'center',
-    //     alignItems: 'center',
-    //     fontSize: 40,
-    //     color: '#010101',
-    //   }}
-    // >
-    //   React template
-    // </div>
-  );
-};
+    if (
+      this.state.contacts.filter(contact => contact.name === name).length > 0
+    ) {
+      alert(`${name} is already in contacts`);
+      return;
+    }
+
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, { id, ...contact }],
+    }));
+  };
+
+  render() {
+    return (
+      <ContactForm onSubmit={this.addContact} />
+
+      // <div
+      //   style={{
+      //     height: '100vh',
+      //     display: 'flex',
+      //     justifyContent: 'center',
+      //     alignItems: 'center',
+      //     fontSize: 40,
+      //     color: '#010101',
+      //   }}
+      // >
+      //   React template
+      // </div>
+    );
+  }
+}
+export default App;
