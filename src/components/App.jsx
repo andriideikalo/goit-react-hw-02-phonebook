4c1b52f92c31bf2ee9024d3ebb387489e3713374
andriideikalo <wlada801@gmail.com>
2023-01-18 21:15:43 +0200

    add card

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,26 @@
+// import React, { Component } from 'react';
+import ContactForm from './ContactForms/ContactForm';
+
 export const App = () => {
-  state = {
-    contacts: [],
-    name: '',
-  };
+  // state = {
+  //   contacts: [],
+  //   name: '',
+  // };
 
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React template
-    </div>
+    <ContactForm />
+
+    // <div
+    //   style={{
+    //     height: '100vh',
+    //     display: 'flex',
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //     fontSize: 40,
+    //     color: '#010101',
+    //   }}
+    // >
+    //   React template
+    // </div>
   );
 };
